84094604743bf53ae1d7759b707bf9f84d804953
Maruf Abdullaev <m.abdullaev4188@gmail.com>
2021-01-20 03:26:33 -0500

    Second Commit

--- AlifLesson1HW19.01/Program.cs
+++ AlifLesson1HW19.01/Program.cs
@@ -6,7 +6,43 @@ namespace AlifLesson1HW19._01
     {
         static void Main(string[] args)
         {
+            //task1
+
+            //double a = 16.80, b = 12.40;
+            //double result = Math.Sqrt(a * b);
+            //Console.WriteLine(result);
+
+            //task2
+
+            //double a = 1.40, b = -5.50, c = 0.60;
+            //double ac = Math.Abs(c - a), bc = Math.Abs(c - b);
+            //Console.WriteLine(ac + bc);
+
+            //task3
+
+            //double x1 = -6.20, y1 = 5.20;
+            //double x2 = 2.10, y2 = 9.80;
+            //double result = Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2 - y1),2));
+
+            //task4
+
+            //int a = 41;
+            //int x = a / 10;
+            //int y = a % 10;
+            //int result = y * 10 + x;
+
+            //task5
+
+            //int n = 10985 / 60;
+            //Console.WriteLine(n);
+
+            //task6
+
+            //int k = 202 % 7;
+            //Console.WriteLine(k);
+
             
+            Console.ReadKey();
         }
     }
 }
